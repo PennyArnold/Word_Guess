a48661a1e46c4746c000c1e37e9a8a6027c8eb25
Penny <radiogaga66@gmail.com>
2019-03-07 15:11:32 -0600

    had issues with Word.js - corrected errors with functions

--- Letter.js
+++ Letter.js
@@ -19,3 +19,5 @@ function Letter(letter) {
         }
     }
 };
+
+module.exports = Letter;
--- Word.js
+++ Word.js
@@ -5,18 +5,27 @@ var Letter = require("./letter.js");
 
 function Word(word) {
     // An array of new Letter objects representing the letters of the underlying word
-    this.word = ["frustrating", "debug", "node", "sequel", "coding", "genius"];
+    this.word = [];
+    for (i = 0; i < word.length; i++) {
+        this.word.push(new Letter(word.charAt(i)));
+    }
 
 
     // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-    this.wordAnswer = function () {
-        for (var i = 0; i > this.word.length; i++) {
-            return parseInt(this.blank);
-        }
-    }
+    this.string = function () {
+        var string = "";
+        this.arr.forEach(function (e) {
+            string += e.blank();
+        });
+        return string;
+    };
 
     // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
-    this.wordShow = function () {
-        return parseInt(this.letterCorrect);
+    this.checkLetter = function(GuessedLetter) {
+        this.arr.forEach(function(e) {
+          e.letterCorrect(GuessedLetter);
+        });
+      };
     }
-};
+    
+    module.exports = Word;
